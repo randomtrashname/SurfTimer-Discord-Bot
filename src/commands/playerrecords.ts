3a95c3339fd0be1e2d5fcff67949bf1c4d55bcfe
randomtrashname <romijn.thimo@gmail.com>
2022-09-12 20:38:22 +0200

    Move style lookup in WHERE clause

--- src/commands/playerrecords.ts
+++ src/commands/playerrecords.ts
@@ -55,7 +55,7 @@ async function cmdCallback(
         }
     })
 
-    const res1 = await prisma.$queryRaw<ck_playertimes[]>`SELECT mapname FROM ck_playertimes a WHERE runtimepro = (SELECT MIN(runtimepro) FROM ck_playertimes b WHERE a.mapname = b.mapname) AND steamid=${x.steamid} AND style=0`
+    const res1 = await prisma.$queryRaw<ck_playertimes[]>`SELECT mapname FROM ck_playertimes a WHERE runtimepro = (SELECT MIN(runtimepro) FROM ck_playertimes b WHERE a.mapname = b.mapname AND style=0) AND steamid=${x.steamid}`
     if (!res1 || res1.length == 0) {
         return `${player.personaname} has no records.`;
     }
